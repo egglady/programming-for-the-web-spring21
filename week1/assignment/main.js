02ecd8184e21eaa0deea97f68674affbb1f0fc45
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-22 17:50:43 -0600

    assignment files

---
+++ week1/assignment/main.js
@@ -0,0 +1,10 @@
+function setup ()  {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw () {
+    background(200);
+    ellipse(500, 100, 100, 100);
+    ellipse(500, 250, 200, 200);
+    ellipse(500, 500, 300, 300);
+}
