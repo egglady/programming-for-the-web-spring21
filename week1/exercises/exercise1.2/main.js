afd53e2dc26c53adbba771451a9c06df430fb675
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-21 19:23:53 -0600

    change ellipse color

--- week1/exercises/exercise1.2/main.js
+++ week1/exercises/exercise1.2/main.js
@@ -1,11 +1,15 @@
-function setup () {
+function setup() {
     createCanvas(400, 400);
+
 }
 
-function draw () {
+function draw() {
+    background(220);
+
     if (mouseIsPressed) {
         ellipse(mouseX, mouseY, 80, 80);
     }
-  
+    let c = color('hsl(15, 70%, 50%)');
+    fill(c);
+    noStroke();
 }
-
