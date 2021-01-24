7152cb42c249662a5b8fbbebbf5fd6f819439a58
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-23 21:46:35 -0600

    stroke weight added

--- week1/assignment/main.js
+++ week1/assignment/main.js
@@ -7,6 +7,7 @@ function draw () {
     ellipse(500, 100, 100, 100);
     ellipse(500, 250, 200, 200);
     ellipse(500, 500, 300, 300);
+    strokeWeight(4);
     line(354, 390, 412, 201);
     line(587, 201, 635, 300);
     line(635, 300, 552, 362);
