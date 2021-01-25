e435cf6a6b19060b0b66c4926829279fdf39af85
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-24 19:56:54 -0600

    red bowtie

--- week1/assignment/main.js
+++ week1/assignment/main.js
@@ -4,6 +4,7 @@ function setup ()  {
 
 function draw () {
     background(200);
+    fill(255);
     ellipse(500, 100, 100, 100);
     ellipse(500, 250, 200, 200);
     ellipse(500, 500, 300, 300);
@@ -11,6 +12,8 @@ function draw () {
     line(354, 390, 412, 201);
     line(587, 201, 635, 300);
     line(635, 300, 552, 362);
+    // bowtie
+    fill(255, 65, 0);
     triangle(500, 160, 550, 140, 550, 175);
     triangle(500, 160, 450, 140, 450, 175);
 }
