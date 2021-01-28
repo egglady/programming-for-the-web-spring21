1c51fbe19b80bebbd8ee03beb45681bf0592e18f
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-28 14:27:00 -0600

    added properties and window.prompt

--- week2/exercise2.2/game.js
+++ week2/exercise2.2/game.js
@@ -1,7 +1,10 @@
-const myGames = [
-    "Clue",
-    "Set",
-    "Jenga",
-    "Codenames"
-];
+const newGame = window.prompt("I have four games in my Collection. Pick a number between 1 and 4 and I'll tell you about the game you've chosen.");
+
+const myGames = {
+    title: 'Clue', type: 'eggs', numberOfPlayers: 6, rating: 1, shortDescription:'murder mystery',
+    title: 'Set', type: 'bacon', numberOfPlayers: 4, rating: 9, shortDescription:'visual perception',
+    title: 'Jenga', type: 'toast', numberOfPlayers: 10, rating: 7, shortDescription:'stacking puzzle',
+    title: 'Codenames', type: 'butter', numberOfPlayers: 12, rating: 8, shortDescription:'team guessing game like Battleship'
+};
+
 console.log(myGames);
