0fb6233864a8c4b4f5093355a93aa38d4c6c25c7
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-28 18:50:58 -0600

    edit alert

--- week2/exercise2.2/game.js
+++ week2/exercise2.2/game.js
@@ -3,13 +3,13 @@ const game = window.prompt("I have four games in my Collection. Pick a number be
 const myGames = [
     {title: 'Clue', type: 'board game', numberOfPlayers: 6, rating: 8, shortDescription: 'murder mystery'},
     {title: 'Set', type: 'card game', numberOfPlayers: 4, rating: 10, shortDescription: 'game of visual perception'},
-    {title: 'Jenga', type: 'puzzle', numberOfPlayers: 6, rating: 4, shortDescription: 'stacking puzzle'},
-    {title: 'Codenames', type: 'party game', numberOfPlayers: 8, rating: 7, shortDescription: 'team guessing game like Battleship'}
+    {title: 'Jenga', type: 'puzzle', numberOfPlayers: 6, rating: 4, shortDescription: 'stacking puzzle with a loud crash that is beloved by children'},
+    {title: 'Codenames', type: 'party game', numberOfPlayers: 8, rating: 7, shortDescription: 'team guessing game like Battleship, in which players try to get their teammates to guess certain words'}
 
 ];
 
 console.log('title', myGames[0].title);
 
 window.alert(
-    'You selected' + myGames[game - 1].title + 'which is a ' + myGames[game - 1].type + 'of game and played with up to ' + myGames[game - 1].numberOfPlayers + '. People often describe this game as a ' + myGames[game - 1].shortDescription
+    'You selected ' + myGames[game - 1].title + ' which is a ' + myGames[game - 1].type + ' and played with up to ' + myGames[game - 1].numberOfPlayers + ' players. People often describe this game as a ' + myGames[game - 1].shortDescription + '.'
     );
