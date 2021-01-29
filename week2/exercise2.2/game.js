7843070de8825be2e87b9e8563bde80c3830545f
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-28 18:22:32 -0600

    update properties

--- week2/exercise2.2/game.js
+++ week2/exercise2.2/game.js
@@ -1,10 +1,11 @@
-const newGame = window.prompt("I have four games in my Collection. Pick a number between 1 and 4 and I'll tell you about the game you've chosen.");
+// const newGame = window.prompt("I have four games in my Collection. Pick a number between 1 and 4 and I'll tell you about the game you've chosen.");
 
-const myGames = {
-    title: 'Clue', type: 'eggs', numberOfPlayers: 6, rating: 1, shortDescription:'murder mystery',
-    title: 'Set', type: 'bacon', numberOfPlayers: 4, rating: 9, shortDescription:'visual perception',
-    title: 'Jenga', type: 'toast', numberOfPlayers: 10, rating: 7, shortDescription:'stacking puzzle',
-    title: 'Codenames', type: 'butter', numberOfPlayers: 12, rating: 8, shortDescription:'team guessing game like Battleship'
-};
+const myGames = [
+    {title: 'Clue', type: 'board game', numberOfPlayers: 6, rating: 8, shortDescription: 'murder mystery'},
+    {title: 'Set', type: 'card game', numberOfPlayers: 4, rating: 10, shortDescription: 'visual perception'},
+    {title: 'Jenga', type: 'puzzle', numberOfPlayers: 6, rating: 4, shortDescription: 'stacking puzzle'},
+    {title: 'Codenames', type: 'party game', numberOfPlayers: 8, rating: 7, shortDescription: 'team guessing game like Battleship'}
+
+];
 
 console.log(myGames);
