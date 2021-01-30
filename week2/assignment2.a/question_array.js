1b9d6d6fdefd4aa5c79fc02a30e4283644d7076c
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-30 16:27:10 -0600

    update question and prompt

--- week2/assignment2.a/question_array.js
+++ week2/assignment2.a/question_array.js
@@ -1,14 +1,14 @@
 const myQuiz = [
-    {question: 'What color do you get from mixing red and blue?', answer: 'purple'},
-    {question: 'What color do you get from mixing red and yellow?', answer: 'orange'},
-    {question: 'What color do you get from mixing blue and yellow?', answer: 'green'},
-    {question: 'What color do you get from mixing red and white?', answer: 'pink'},
-    {question: 'What color do you get from mixing black and white?', answer: 'grey'}
+    {question: 'What color do you get from mixing red and blue? Reply with  common color name.', answer: 'purple'},
+    {question: 'What color do you get from mixing red and yellow? Reply with  common color name.', answer: 'orange'},
+    {question: 'What color do you get from mixing blue and yellow? Reply with  common color name.', answer: 'green'},
+    {question: 'What color do you get from mixing red and white? Reply with  common color name.', answer: 'pink'},
+    {question: 'What color do you get from mixing black and white? Reply with  common color name.', answer: 'grey'}
 ];
 
 console.log(myQuiz);
 
-const randomIdx = Math.round(Math.random() * myQuiz.length - 1);
+const randomIdx = Math.round(Math.random() * (myQuiz.length - 1));
 console.log(myQuiz[randomIdx].question);
 
-const randomQuestion = prompt(myQuiz[0].question);
+const random = prompt(myQuiz[randomIdx].question);
