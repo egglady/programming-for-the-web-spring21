23fe1cbaca8c845df74eb519160097faef297483
Sorcha <64171410+egglady@users.noreply.github.com>
2021-01-30 17:31:04 -0600

    personalized questions

--- week2/assignment2.a/question_array.js
+++ week2/assignment2.a/question_array.js
@@ -1,11 +1,11 @@
 
 // array
 const myQuiz = [
-    {question: 'What color do you get from mixing red and blue? Reply with  common color name.', answer: 'purple'},
-    {question: 'What color do you get from mixing red and yellow? Reply with  common color name.', answer: 'orange'},
-    {question: 'What color do you get from mixing blue and yellow? Reply with  common color name.', answer: 'green'},
-    {question: 'What color do you get from mixing red and white? Reply with  common color name.', answer: 'pink'},
-    {question: 'What color do you get from mixing black and white? Reply with  common color name.', answer: 'grey'}
+    {question: 'What kind of eggs are boiled in their shell until the yolk is solid? Reply with the common cooking method.', answer: 'hard boiled'},
+    {question: 'What kind of eggs are cooked in a skillet, have a runny yolk, and are not flipped over? Reply with the common cooking method.', answer: 'sunny side up'},
+    {question: 'What kind of eggs are boiled outside of their shell? Reply with the common cooking method.', answer: 'poached'},
+    {question: 'What kind of eggs are hardboiled, halved, and stuffed with a paste of the yolk, mayonnaise, and mustard? Reply with the common cooking method.', answer: 'deviled'},
+    {question: 'What kind of eggs are beaten, then cooked in a skillet with butter (and often milk, salt, and pepper)? Reply with the common cooking method.', answer: 'scrambled'}
 ];
 
 console.log(myQuiz);
