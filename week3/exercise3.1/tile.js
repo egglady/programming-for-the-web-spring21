c765b708d523ad90e58043d3b3d1daab0d45098a
Sorcha <64171410+egglady@users.noreply.github.com>
2021-02-04 10:34:53 -0600

    tile updates, loop added

--- week3/exercise3.1/tile.js
+++ week3/exercise3.1/tile.js
@@ -1,97 +1,92 @@
 function setup() {
- createCanvas(200, 200);
+  createCanvas(200, 200);
+  noLoop();
 }
 
 function createTile() {
+  translate(0, 0);
+  // square
+  noStroke();
+  fill(40, 250, 130, 180);
+  // 40, 250, 130
+  square(0, 0, 200);
+  // checker squares
+  noStroke();
+  fill(210, 240, 180, 220);
+  rect(5, 5, 90, 90);
 
-    // checker background
-    noStroke();
-    fill(195, 240, 180);
-    rect(0, 0, 100, 100);
+  fill(26, 89, 13,);
+  rect(105, 5, 90, 90);
 
-    fill(26, 89, 13);
-    rect(100, 0, 100, 100);
+  rect(5, 105, 90, 90);
 
-    rect(0, 100, 100, 100);
+  fill(210, 240, 180, 220);
+  rect(105, 105, 90, 90);
 
-    fill(195, 240, 180);
-    rect(100, 100, 100, 100);
+  // hexagon
+  fill(255);
+  beginShape ();
 
-    // hexagon
-    fill(255);
-    beginShape ();
-    vertex(88, 80);
-    vertex(112, 80);
-    vertex(125, 100);
-    vertex(112, 120);
-    vertex(88, 120);
-    vertex(75, 100);
-    vertex(88, 80);
-    endShape();
+  // reusing same values
+  var a = 88;
+  var b = 80;
+  var c = 112;
+  var d = 125;
+  var e = 100;
+  var f = 120;
+  var g = 75;
 
-    // triangles top
-    noStroke();
-    fill(0, 128, 47);
-    triangle(100, 30, 100, 80, 88, 80);
-    fill(0, 190, 40);
-    triangle(100, 30, 100, 80, 112, 80);
-    // triangles at top right
-    fill(0, 128, 47);
-    triangle(157, 60, 118, 90, 112, 80);
-    fill(0, 190, 40);
-    triangle(157, 60, 118, 90, 125, 100);
-    // triangles at bottom right
-    fill(0, 128, 47);
-    triangle(157, 140, 118, 110, 125, 100);
-    fill(0, 190, 40);
-    triangle(157, 140, 118, 110, 112, 120);
+  vertex(88, 80);
+  vertex(112, 80);
+  vertex(125, 100);
+  vertex(112, 120);
+  vertex(88, 120);
+  vertex(75, 100);
+  vertex(88, 80);
+  endShape();
 
-    // triangles at bottom
-    fill(0, 128, 47);
-    triangle(100, 170, 100, 120, 112, 120);
-    fill(0, 190, 40);
-    triangle(100, 170, 100, 120, 88, 120);
+  // triangles top
+  noStroke();
+  fill(0, 128, 47);
+  triangle(e, 30, e, b, a, b);
+  fill(0, 190, 40);
+  triangle(e, 30, e, b, c, b);
+  // triangles at top right
+  fill(0, 128, 47);
+  triangle(157, 60, 118, 90, c, b);
+  fill(0, 190, 40);
+  triangle(157, 60, 118, 90, d, e);
+  // triangles at bottom right
+  fill(0, 128, 47);
+  triangle(157, 140, 118, 110, d, e);
+  fill(0, 190, 40);
+  triangle(157, 140, 118, 110, c, f);
 
-    // triangles at bottom left
-    fill(0, 128, 47);
-    triangle(42, 140, 82, 112, 88, 120);
-    fill(0, 190, 40);
-    triangle(42, 140, 82, 112, 75, 100);
+  // triangles at bottom
+  fill(0, 128, 47);
+  triangle(e, 170, e, f, c, f);
+  fill(0, 190, 40);
+  triangle(e, 170, e, f, a, f);
 
-    //triangles at top left
-    fill(0, 128, 47);
-    triangle(42, 60, 82, 90, 75, 100);
-    fill(0, 190, 40);
-    triangle(42, 60, 82, 90, 88, 80);
+  // triangles at bottom left
+  fill(0, 128, 47);
+  triangle(42, 140, 82, c, a, f);
+  fill(0, 190, 40);
+  triangle(42, 140, 82, c, g, e);
 
-    // circle
-    noFill();
-    stroke('white');
-    strokeWeight(2);
-    circle(100, 100, 150);
+  //triangles at top left
+  fill(0, 128, 47);
+  triangle(42, 60, 82, 90, g, e);
+  fill(0, 190, 40);
+  triangle(42, 60, 82, 90, a, b);
+
+  // circle
+  noFill();
+  stroke('white');
+  strokeWeight(2);
+  circle(100, 100, 150);
 }
 // call the function
 function draw() {
     createTile();
 }
-
-
-// use the noLoop method to stop the draw Loop
-
-
-// translate(0, 0);
-// fill('blue');
-// rect(0, 0, 200, 200);
-// stroke('white');
-// strokeWeight(5);
-// line(0, 0, 200, 200);
-// line(200, 0, 0, 200);
-// noStroke();
-// fill('yellow');
-// circle(100, 50, 50);
-// circle(150, 100, 50);
-// circle(100, 150, 50);
-// circle(50, 100, 50);
-// noFill();
-// stroke('purple');
-// circle(100, 100, 100); 
