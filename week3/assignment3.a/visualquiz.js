9827d52f5d2c1a6e6bce8d117bc679350853f113
Sorcha <64171410+egglady@users.noreply.github.com>
2021-02-07 17:05:19 -0600

    css updates

--- week3/assignment3.a/visualquiz.js
+++ week3/assignment3.a/visualquiz.js
@@ -2,8 +2,9 @@ let submitAnswerButton
 let questionInput;
 let currentQuestion;
 let response;
-let responseColor = 'green';
+let responseColor = '#0965CF';
 let wrongResponse = Number(0);
+let rightResponse = Number(0);
 let heading;
 let myQuiz = [
   { question: 'What kind of eggs are boiled in their shell until the yolk is solid?', answer: 'hard boiled'},
@@ -14,13 +15,13 @@ let myQuiz = [
 ];
 function next () {
   if(myQuiz.length < 1) {
-    window.alert('you won');
+    window.alert('You cracked it!');
     location.reload();
     return;
   }
 
   if (wrongResponse > 4) {
-    window.alert('you lost');
+    window.alert('Darnit! Looks like you\'re a rotten egg');
     location.reload();
     return;
   }
@@ -29,17 +30,18 @@ function next () {
 }
 function checkQuestion() {
   if (currentQuestion.answer === questionInput.value()) {
-    // remove correct answer from array, you could also splice it out
+    // remove correct answer from array
     myQuiz = myQuiz.filter(myQuizObj => {
       return currentQuestion.answer !== myQuizObj.answer;
     });
     // this is the correct answer condition
     response = 'correct! next question';
-    responseColor = 'green'
+    responseColor = '#0965CF'
+    rightResponse++;
   } else {
     // this is the wrong answer condition
     response = 'oops, that wasn\'t quite right! Try another';
-    responseColor = 'red';
+    responseColor = '#333';
     wrongResponse++;
   }
   currentQuestion = next();
@@ -60,23 +62,29 @@ let message = currentQuestion.question;
 // console.log(next());
 
 function setup() {
-  createCanvas(1400, 1000);
+  createCanvas(windowWidth, windowHeight);
   heading = createElement('h1', ['Eggcellent Questions']);
   heading.position(100, 100);
   questionInput = createInput('');
-  questionInput.size(250, 20);
-  questionInput.position(100, 325);
+  questionInput.size(250, 60);
+  questionInput.position(100, 250);
   submitAnswerButton = createButton('Submit');
-  submitAnswerButton.size(250, 20);
+  submitAnswerButton.size(200, 40);
   submitAnswerButton.mousePressed(checkQuestion);
-  submitAnswerButton.position(100, 350);
+  submitAnswerButton.position(100, 325);
+  startOverButton = createButton('Start Over');
+  startOverButton.size(200, 40);
+  startOverButton.mousePressed(startOver);
+  startOverButton.position(100, 450);
 }
 
 function draw() {
   background('#F5C95D');
   fill('white');
-  textSize(20);
-  text(message, 100, 300);
+  textSize(22);
+  text(message, 100, 220);
+  textSize(18);
   fill(responseColor);
-  text(response, 100, 400);
+  text(response, 100, 375);
+  text(rightResponse + ' correct answers', 100, 510);
 }
