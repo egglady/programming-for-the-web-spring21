627e5ee19bfed68dae07f5d245f1a4bf52075606
Sorcha <64171410+egglady@users.noreply.github.com>
2021-02-10 16:43:41 -0600

    ex 4.1 start

---
+++ week4/dom-manipulation.js
@@ -0,0 +1,14 @@
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
+
+// create the heading element
+const heading = document.createElement('h2');
+heading.innerHTML = 'Llama you glad I didn\'t say banana';
+
+// attach to DOM
+heading.addEventListener('click', handleHeadingClick); // see this function below
+
+myEmptyDiv.appendChild(heading);
+function handleHeadingClick () {
+  document.body.style.backgroundColor = '#999acf';
+  console.log('duck duck llama! You clicked on the llama heading');
+}
