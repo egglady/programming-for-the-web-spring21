9182c4b351953b26d7b7713eb766526d7949d584
Sorcha <64171410+egglady@users.noreply.github.com>
2021-02-18 21:39:48 -0600

    removed grid, made Card class with flip

--- week5/5.a/memorygame.js
+++ week5/5.a/memorygame.js
@@ -1,38 +1,59 @@
-const rectWidth = 120;
-const rectHeight = 220;
-let myCards = [];
-let startingX = 100;
-let startingY = 100;
-let startingId;
+let myCard;
+
+const DOWN = 'down';
+const UP = 'up';
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  for (let k = 0; k < 2; k++) {
-    for (let i = 0; i < 8; i++) {
-      rect(startingX, startingY, rectWidth, rectHeight);
-        myCards.push({ x: startingX, y: startingY, id: startingId }); // pushes these objects into our array, id: i is the loop variable
-        startingX += 150;
-        startingId++;
-    }
-    startingY += 250;
-    startingX = 100;
-  }
-  console.log(myCards);
+  myCard = new Card();
 }
 
 function mousePressed() {
-  for (let j = 0; j < myCards.length; j++) {
-      let distance = dist(mouseX, mouseY, myCards[j].x, myCards[j].y);
-      if (distance < rectWidth - 30) {
-    }
-  }
+  console.log(myCard.didHit(mouseX, mouseY));
 }
 
-function draw () {
-  if(startingId > 0) {
-      noLoop();
-      fill(200);
-      textSize(20);
+// function draw () {
+//   if(startingId > 0) {
+//       noLoop();
+//       fill(200);
+//       textSize(20);
+//   }
+// }
+
+class Card {
+  constructor () {
+    this.x = 100;
+    this.y = 100;
+    this.width = 80;
+    this.height = 100;
+    this.face = DOWN;
+    this.show();
+  }
+  show () {
+    if (this.face === DOWN) {
+      fill(200, 200, 200);
+      rect(this.x, this.y, this.width, this.height, 5);
+    } else {
+      fill(255, 0, 0);
+      rect(this.x, this.y, this.width, this.height, 5);
+    }
+  }
+
+  didHit (mouseX, mouseY) {
+    if((mouseX >= this.x && mouseX <= this.x + this.width) && (mouseY >= this.y && mouseY <= this.y + this.height)) {
+      this.flip();
+      return true;
+    } else {
+        return false;
+    }
+  }
+  flip () {
+    if (this.face === DOWN) {
+      this.face = UP;
+    } else {
+      this.face = DOWN;
+    } 
+    this.show();
   }
 }
