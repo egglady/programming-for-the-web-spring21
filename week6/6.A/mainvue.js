0d41cf5705b5388cc05f34cd4ec1a490eb417e0b
Sorcha <64171410+egglady@users.noreply.github.com>
2021-02-28 14:26:01 -0600

    create llama facts

--- week6/6.A/mainvue.js
+++ week6/6.A/mainvue.js
@@ -1,7 +1,18 @@
-var list = new Vue({
-    el: #list,
+var fact = new Vue({
+    el: '#fact',
     data: {
-        
+        llamaHum: 'Llamas communicate with each other by humming.',
+        llamaAura: 'Llamas have a soothing aura about them and can be trained as professional therapy animals.',
+        llamaSpit: 'Llama spit to establish a pecking order, but they rarely spit on humans.',
+        llamaAlpaca: 'Llamas are friendlier to humans than alpacas.',
+        llamaFertilizer: 'Llama poop, which we all know has no odor, also is a great fertilizer and fire starter.',
+        llamaHeight: 'Llamas are on average between 5 ft 6 in and 5 ft 9 in, close in height to an average human, so llamas would make a great opponent in a staring contest.',
+        llamaAngry: 'When one llama is angry at another, they will stick out their tongue to express their dislike.',
+        llamaEmperor: 'The Emperor’s New Groove is an animated comedy movie set in the Inca kingdom, in which selfish and egotistical emperor Kuzco is turned into a llama by his advisor, Yzma. After being removed from his palace, he goes on a quest to reclaim his throne and return to human form with the help of a kindly peasant, Pacha. This movie is the second greatest film of all time, behind The Godfather I.',
+        llamaGodfather: 'There are no llamas with speaking roles in The Godfather film series.',
+        llamaEgg: 'No, llama’s do not lay eggs.'
     }
 
 })
+
+// facts: ['Llamas communicate by humming.', 'Llamas have a soothing aura about them and can be trained as professional therapy animals.', 'Llamas are friendlier to humans than alpacas.', 'Llama spit to establish a pecking order, but they rarely spit on humans.', 'Llama poop, which we all know has no odor, also is a great fertilizer and fire starter.']
