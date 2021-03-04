ed08343b57d8a54aff88093e27477a9545e2871f
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-03 21:24:13 -0600

    add uls, begin css with flex

--- week7/7.1/main.js
+++ week7/7.1/main.js
@@ -1,7 +1,13 @@
-var links = new Vue ({
-    el: '#links',
+var llama = new Vue ({
+    el: '#llama',
     data: {
-        llamaWiki: 'https://en.wikipedia.org/wiki/Llama',
+        llamaWiki: 'https://en.wikipedia.org/wiki/Llama'
+    }
+})
+
+var alpaca = new Vue ({
+    el: '#alpaca',
+    data: {
         alpacaWiki: 'https://en.wikipedia.org/wiki/Alpaca'
     }
 })
