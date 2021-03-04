62fedfebf436885b0f856d43b946b1538652da81
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-03 20:23:50 -0600

    v-bind links to wikipedia

---
+++ week7/7.1/main.js
@@ -0,0 +1,7 @@
+var links = new Vue ({
+    el: '#links',
+    data: {
+        llamaWiki: 'https://en.wikipedia.org/wiki/Llama',
+        alpacaWiki: 'https://en.wikipedia.org/wiki/Alpaca'
+    }
+})
