aa467686223193b315292d878b51ad6206c75b8c
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-04 21:01:41 -0600

    added -v-bind: style, facts

--- week7/7.1/main.js
+++ week7/7.1/main.js
@@ -1,13 +1,28 @@
 var llama = new Vue ({
     el: '#llama',
     data: {
-        llamaWiki: 'https://en.wikipedia.org/wiki/Llama'
+        currentRoute: 'big',
+        llamaWiki: 'https://en.wikipedia.org/wiki/Llama',
+        linkColor: {
+            color: 'rgb(52, 97, 209, .7)'
+        },
+        baseStyle: {
+            color: 'rgb(52, 97, 209)'
+        }
     }
 })
 
 var alpaca = new Vue ({
     el: '#alpaca',
     data: {
-        alpacaWiki: 'https://en.wikipedia.org/wiki/Alpaca'
+        currentRoute: 'big',
+        alpacaWiki: 'https://en.wikipedia.org/wiki/Alpaca',
+        linkColor: {
+            color: 'rgba(194, 77, 27, .7)'
+        },
+        baseStyle: {
+            color: 'rgb(194, 77, 27)',
+            'padding-right': '2rem'
+        }
     }
 })
