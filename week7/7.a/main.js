b36735d86ed36368f242cc2927a3918f7c8271da
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-06 20:33:39 -0600

    add images to array

--- week7/7.a/main.js
+++ week7/7.a/main.js
@@ -7,6 +7,7 @@ var vm = new Vue ({
         artist: 'Unknown artist',
         medium: 'coral, brass, niello, silver, iron, gold',
         date: 'late 16th century',
+        image: 'images/knife.jpg',
         onView: true
       },
       {
@@ -14,6 +15,7 @@ var vm = new Vue ({
         artist: 'Hans Ledwinka',
         medium: 'all-metal construction',
         date: '1948, designed 1936',
+        image: 'images/tatra.jpg',
         onView: true
       },
       {
@@ -21,6 +23,7 @@ var vm = new Vue ({
         artist: 'William Grigg',
         medium: 'silver and wood',
         date: 'c. 1791 - 1794',
+        image: 'images/teapot.jpg',
         onView: true
       },
       {
@@ -28,6 +31,7 @@ var vm = new Vue ({
         artist: 'Unknown artist',
         medium: 'gold',
         date: '19th - 20th century',
+        image: 'images/pambadam-earrings.jpg',
         onView: true
       },
       {
@@ -35,6 +39,7 @@ var vm = new Vue ({
         artist: 'Walter Dorwin Teague',
         medium: 'mirrored glass, chrome, painted wood, electrical components',
         date: 'c. 1937',
+        image: 'images/nocturne-radio.jpg', 
         onView: true
       }
     ]
