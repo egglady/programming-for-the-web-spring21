dd8f0f8103ef96be82b05507dd5d8065d55942a6
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-07 20:08:52 -0600

    v-bind links to the items

--- week7/7.a/main.js
+++ week7/7.a/main.js
@@ -11,6 +11,7 @@ var vm = new Vue ({
         image: 'images/knife-400.jpg',
         imageAlt: 'A knife and spear set with ornate floral inlaid designs on the hilt and sides of the knife, with large spiky red coral handles on a black background.',
         onView: true,
+        link:'https://collections.artsmia.org/art/109578/knife-italy'
       },
       {
         title: 'Tatra T87 four door sedan',
@@ -19,7 +20,8 @@ var vm = new Vue ({
         date: '1948, designed 1936',
         image: 'images/tatra-400.jpg',
         imageAlt: 'A silver car with distinctive rear dorsal fins with no passengers in a museum gallery.',
-        onView: true
+        onView: true,
+        link: 'https://collections.artsmia.org/art/98653/tatra-t87-four-door-sedan-hans-ledwinka'
       },
       {
         title: 'Teapot',
@@ -28,7 +30,8 @@ var vm = new Vue ({
         date: 'c. 1791 - 1794',
         image: 'images/teapot-400.jpg',
         imageAlt: 'An oval silver teapot with a black handle and an engraved design of a crest on its body.',
-        onView: true
+        onView: true,
+        link: 'https://collections.artsmia.org/art/745/teapot-willliam-grigg'
       },
       {
         title: 'Pambadam Earrings',
@@ -37,7 +40,8 @@ var vm = new Vue ({
         date: '19th - 20th century',
         image: 'images/pambadam-earrings-400.jpg',
         imageAlt: 'Two geometric gold earrings in the abstract shape of a cobray laying eggs on her nest.',
-        onView: true
+        onView: true,
+        link: 'https://collections.artsmia.org/art/4380/pambadam-earrings-india'
       },
       {
         title: '\"Nocturne\" radio',
@@ -46,7 +50,8 @@ var vm = new Vue ({
         date: 'c. 1937',
         image: 'images/nocturne-radio-400.jpg',
         imageAlt: 'A furniture-sized futuristic circular radio with a bright blue mirrored glass facade.',
-        onView: true
+        onView: true,
+        link: 'https://collections.artsmia.org/art/6783/nocturne-radio-walter-dorwin-teague'
       }
     ]
   }
