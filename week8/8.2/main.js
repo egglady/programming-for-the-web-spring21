08ea0db5a26f3c6aab441f02f8035d1c7d6c90be
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-11 20:27:03 -0600

    add functions

--- week8/8.2/main.js
+++ week8/8.2/main.js
@@ -1,11 +1,34 @@
 var cal = new Vue ({
-    el: '#calcApp',
-    data: {
-        firstNum: null,
-        secondNum: null,
-        operation: 'add',
-        total: null
-    },
-    methods: {
+  el: '#calcApp',
+  data: {
+    firstNum: null,
+    secondNum: null,
+    operation: 'add',
+    total: null
+  },
+  methods: {
+    calculate: (num1, num2) => {
+      // switch is similar to an if statement but with specific strings/values to check against
+      switch (cal.operation) {
+        case 'add':
+          cal.total = num1 + num2;
+          // they will concatenate unless
+          break;
+        case 'subtract':
+          cal.total = num1 - num2;
+          break;
+        case 'multiply':
+          cal.total = num1 * num2;
+          break;
+        case 'divide':
+          cal.total = num1 / num2;
+          break;
+
+        default:
+          window.alert('unexpected');
+          break;
+      }
+      cal.firstNum = cal.secondNum = null;
     }
+  }
 })
