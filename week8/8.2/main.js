3bf424f7a5dc6a4a66eaa05799f0288111dca258
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-11 20:08:44 -0600

    start files, form, operations

---
+++ week8/8.2/main.js
@@ -0,0 +1,11 @@
+var cal = new Vue ({
+    el: '#calcApp',
+    data: {
+        firstNum: null,
+        secondNum: null,
+        operation: 'add',
+        total: null
+    },
+    methods: {
+    }
+})
