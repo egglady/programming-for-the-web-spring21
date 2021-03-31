511b051114a939b0354f6e5d4da271c3d058639e
Sorcha <64171410+egglady@users.noreply.github.com>
2021-03-30 20:33:25 -0500

    edit file structure

--- week10/ex2/script.js
+++ week10/ex2/script.js
@@ -1,15 +1,19 @@
-const vm = new Vue ({
+const vm = new Vue({
   el: '#musicApp',
   data: {
     tracks: []
   },
   mounted () {
-       // think of this kind of like draw from p5.js
     axios
-      .get('./data/music-list.json')// start with ./ for start in the directory you're currently in
-      // Promise syntax is the following .then
+      .get('./data/music-list.json')
       .then(response => {
         console.log('response', response);
+        vm.tracks = response.data;
       });
   }
 })
+
+       // think of this kind of like draw from p5.js
+
+       // start with ./ for start in the directory you're currently in
+      // Promise syntax is the following .then
