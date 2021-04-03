6caf82508d2a8ded3099be773054f08b1819a15f
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-03 13:56:29 -0500

    add the for loop

--- week10/10.A/script.js
+++ week10/10.A/script.js
@@ -7,19 +7,25 @@ Vue.component('medieval-artwork', {
 
 
 var vm = new Vue({
-    el: '#museumApp',
-    data: {
-       artObjects: []
-    },
-    mounted: function() {
-        let responseArray = [] // holds response from first axios.get
-        let baseUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
-        axios
-        .get(baseUrl + '?departmentIds=17')
-        .then(response => {
-            console.log('response', response);
-            vm.artworks = response.data;
-            console.log(vm.artworks);
-        })
-    }
+  el: '#museumApp',
+  data: {
+    artObjects: []
+  },
+  mounted: function() {
+    let responseArray = []; // holds response from first axios.get
+    let baseUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+    axios
+      .get(baseUrl + '?departmentIds=17')
+      .then(response => {
+        responseArray = response.data.objectIDs.slice(0, 19); // take out first 20 of the objectIDs in department 17
+        for(let k = 0; k < responseArray.length; k++) {
+          axios
+            .get(baseUrl + '/' + responseArray[k])
+            .then(art => {
+              console.log('art', art.data);
+              vm.artObjects.push(art.data);
+            })
+        }
+      })
+  }
 });
