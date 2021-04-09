d441c36ccc23ceb82e8b05f04ab2c142e66eaef0
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-09 12:25:46 -0500

    adjust formatting

--- week11/looop.js
+++ week11/looop.js
@@ -1,12 +1,11 @@
-
 function setup() {
     createCanvas(500, 500);
 }
 
 function draw() {
 background(7, 21, 117);
-  for (x = 0; x < width; x+=50) {
-    for (y = 0; y < width; y+=50) {
+  for (x = 0; x < width; x += 50) {
+    for (y = 0; y < width; y += 50) {
         rotate(mouseX / 100.0);
         stroke(255);
         strokeWeight(2);
