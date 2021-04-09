a025f36ae017ca99d598fca4f42c346f91bb36d5
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-09 12:24:53 -0500

    looop exercise

---
+++ week11/looop.js
@@ -0,0 +1,17 @@
+
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+background(7, 21, 117);
+  for (x = 0; x < width; x+=50) {
+    for (y = 0; y < width; y+=50) {
+        rotate(mouseX / 100.0);
+        stroke(255);
+        strokeWeight(2);
+        fill(255, 124, 163);
+        ellipse(x + 25, y + 25, 40, 40);
+    }
+  }
+}
