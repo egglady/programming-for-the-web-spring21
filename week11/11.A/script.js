a94d2160fd55790bf15995d4034bfdf354c56d3e
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-11 17:48:30 -0500

    sketches with notes

--- week11/11.A/script.js
+++ week11/11.A/script.js
@@ -1,6 +1,6 @@
 // PARAMETERIZE
 
-// EYEROLL
+// "EYEROLL"
 
 // vars for eyeroll 1.0
 var angle = 0.0;
@@ -9,22 +9,23 @@ var scalar = 10;
 var speed = 0.04;
 
 // vars for eyeroll 2.0
-var offset2 = 200;
-
-// vars for eyeroll 2.1
-var radius = 40;
-var x2 = -radius;
-var speed2 = 0.5;
-var y2 = 400;
+var startI = 20; // x value
+var stopI = 500;
+var i = startI;
+var step = 0.01;
+var pct = 0.0;
 var angle2 = 0.0;
+var speed2 = 0.02;
+
+// vars for eyeroll 3.0
+var angle3 = 0.0;
+var offset3 = 200;
+var scalar3 = -60;
+var speed3 = 0.05;
 
 
 function setup() {
     createCanvas(500, 500);
-    // frameRate needed for movement?
-    // frameRate(10);
-    ellipseMode(RADIUS);
-
 }
 function draw() {
     background(200);
@@ -37,23 +38,50 @@ function draw() {
     var y = offset + sin(angle) * scalar;
     ellipse(x, y, 20, 20);
     angle += speed;
-    // eyeroll 2.0 ?
-    noStroke();
-    var x = offset + cos(angle) * scalar;
-    var y = offset2 + sin(angle) * scalar;
+  
+    // eyeroll 2.0
+    if (pct < 1.0) {
+        i = startI + ((stopI - startI) * pct);
+        pct += step;
+    }
     fill(255);
-    ellipse(x, y, 50, 50);
+    ellipse(i, 350, 50, 50);
     fill(0);
-    ellipse(x, y, 20, 20);
-    angle += speed;
-    // eyeroll 2.1
-    x2 += speed2; // increase the value of x2
-    fill(255);
-    ellipse(x2, 400, 50, 50);
-    fill(0);
-    // var x = offset + cos(angle2) * scalar;
-    // var y2 = offset + sin(angle2) * scalar;
-    ellipse(x2, y2, 20, 20);
+    ellipse(i, y + 310, 20, 20);
     angle2 += speed2;
+
+    // eyeroll 3.0
+    var m = offset3 + (cos(angle3)) * scalar3;
+    var n = offset3 + (sin(angle3)) * scalar3;
+    noStroke();
+    fill(255);
+    ellipse(m, n, 50, 50)
+    fill(0);
+    ellipse(m + 5, n + 5, 20, 20);
+    angle3 += speed3;
+    scalar3 += speed3;
 }
 
+
+// GRAVEYARD OF OTHER EYEROLLS
+// attempt at eyeroll in which: white ellipse moves, black ellipse both rotates and moves (x is doing two things at once - how to represent that?
+// x2 += speed2; // increase the value of x2
+// fill(255);
+// ellipse(x2, 350, 50, 50);
+// fill(0);
+// var x3 = offset + cos(angle2) * scalar;
+// var y2 = offset + sin(angle2) * scalar;
+// ellipse(x3, y2, 20, 20);
+// // angle2 += speed2;
+
+// eyeroll 2.1 ?
+// noStroke();
+// var x = offset + cos(angle) * scalar;
+// var y = offset2 + sin(angle) * scalar;
+// fill(255);
+// ellipse(x, y, 50, 50);
+// fill(0);
+// ellipse(x, y, 20, 20);
+
+// vars for eyeroll 2.1
+// var offset2 = 200;
