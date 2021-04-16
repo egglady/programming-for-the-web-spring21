5fc8fd82388367a0575d9e2c3868d0d77efb71b7
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-15 19:45:20 -0500

    start sound, add files

---
+++ week12/12.2/script.js
@@ -0,0 +1,14 @@
+function preload() {
+    soundFormats ('wav');
+    airdropSound = loadSound('audio/airdrop');
+    muteSound = loadSound('audio/on_mute');
+    slackSound = loadSound('audio/slack');
+    screenSound = loadSound('audio/see_screen');
+    chimeSound = loadSound('audio/zoom_chime');
+}
+
+function setup() {
+}
+
+function draw () {
+}
