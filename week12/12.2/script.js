9311f9d9237771bcef14c4136d3ed19961acbeb6
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-15 22:28:47 -0500

    finalize sound

--- week12/12.2/script.js
+++ week12/12.2/script.js
@@ -1,23 +1,42 @@
 var airdropSound;
 var slackSound;
 var chimeSound;
-var level;
+let level;
 
 function preload() {
     soundFormats ('wav');
-    airdropSound = loadSound('audio/airdrop.wav');
+    airdropSound = loadSound('audio/airdrop');
     slackSound = loadSound('audio/slack');
+    slackSound.setVolume(0.2);
     chimeSound = loadSound('audio/zoom_chime');
+    chimeSound.setVolume(0.2);
 }
 
 function setup() {
     createCanvas(500, 500);
+    amplitude = new p5.Amplitude();
     airdropSound.play();
-    // slackSound.play();
-    // chimeSound.play();
 }
 
 function draw () {
-    level = airdropSound.getLevel();
+    background('#222');
+
+    level = amplitude.getLevel();
     console.log(level);
+    var diameterX = level * 2500;
+    var diameterY = level * 2500;
+    stroke(100, 0, 0);
+    strokeWeight(4);
+    fill(255, 0, 0);
+    ellipse(250, 250, diameterX + 100, diameterY + 100);
+    if (mouseIsPressed) {
+        chimeSound.play();
+    } else {
+  
+    }
+    if (keyIsPressed) {
+        slackSound.play();
+    }  else {
+
+    }
 }
