4e5f002907f852bccd6da3e5312c876fb65f13f8
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-15 20:31:20 -0500

    update files

--- week12/12.2/script.js
+++ week12/12.2/script.js
@@ -1,9 +1,7 @@
 function preload() {
     soundFormats ('wav');
     airdropSound = loadSound('audio/airdrop');
-    muteSound = loadSound('audio/on_mute');
     slackSound = loadSound('audio/slack');
-    screenSound = loadSound('audio/see_screen');
     chimeSound = loadSound('audio/zoom_chime');
 }
 
