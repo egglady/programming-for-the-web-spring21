e9114b33a1e40bca4057522d8da807bb66fd5d77
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-15 21:15:09 -0500

    p5-sound script added

--- week12/12.2/script.js
+++ week12/12.2/script.js
@@ -1,13 +1,23 @@
+var airdropSound;
+var slackSound;
+var chimeSound;
+var level;
+
 function preload() {
     soundFormats ('wav');
-    airdropSound = loadSound('audio/airdrop');
+    airdropSound = loadSound('audio/airdrop.wav');
     slackSound = loadSound('audio/slack');
     chimeSound = loadSound('audio/zoom_chime');
 }
 
 function setup() {
     createCanvas(500, 500);
+    airdropSound.play();
+    // slackSound.play();
+    // chimeSound.play();
 }
 
 function draw () {
+    level = airdropSound.getLevel();
+    console.log(level);
 }
