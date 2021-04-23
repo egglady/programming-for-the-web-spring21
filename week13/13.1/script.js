69849d294b8c7f5c3fa87404c41e22ff1fe81108
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-23 15:09:10 -0500

    change y vals, update comments

--- week13/13.1/script.js
+++ week13/13.1/script.js
@@ -5,7 +5,7 @@ var pos;
 function setup() {
   createCanvas(550, 550);
   angleMode(DEGREES);
-  slider = createSlider(0, 15, 2); // 1 - default
+  slider = createSlider(0, 15, 2); // 2 is default
   slider2 = createSlider(0, 10, 2);
 
   pos = 0.0;
@@ -13,33 +13,29 @@ function setup() {
 }
 
 function draw () {
-  clear (); // clear the background in draw
+  clear (); // to clear the background in draw
   translate(width/2, height/2);
 
   var mySinVal = sin(pos); 
   var mySinVal2 = sin(pos2); 
 
-  amplified = mySinVal * 30 // sin values aren't larger than one, so multiplying them makes the movement visible
+  amplified = mySinVal * 30 // multiply to make sin value larger (sin <= 1)
   amplified2 = mySinVal2 * 60;
 
   for (x = 0; x <= 360; x = x + 10) {
-    // ellipse(0, 50, 5, 5);
     noStroke(0);
     fill(52, 125, 235);
-    ellipse(amplified, 50, amplified, 10);
+    ellipse(amplified, 50, amplified, 6);
     fill(235, 64, 52);
-    ellipse(amplified2, 95, amplified2, 12);
+    ellipse(amplified2, 100, amplified2, 12);
     fill(245, 212, 66);
-    ellipse(amplified, 140, amplified, 10);
+    ellipse(amplified, 150, amplified, 10);
     fill(235, 127, 226);
-    ellipse(amplified2, 185, amplified2, 16);
+    ellipse(amplified2, 200, amplified2, 16);
     fill(255);
-    ellipse(amplified, 230, amplified, 10);
+    ellipse(amplified, 250, amplified, 6);
     rotate(x);
-  } // need translate too
-
-  // ellipse(width/2, height/2, amplified + 100, amplified + 10);
+  }
   pos = pos + slider.value();
   pos2 = pos2 + slider2.value();
-
 }
