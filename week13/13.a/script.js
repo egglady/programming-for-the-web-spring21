25d290c856dec05eb91490c584da7189e56e03de
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-25 14:09:52 -0500

    update indenting

--- week13/13.a/script.js
+++ week13/13.a/script.js
@@ -8,51 +8,46 @@ let armLength;
 let origin; // loc where the arm is "attached"
 let easing = 0.05;
 var slider;
-
 var sun;
 
-
 function preload() {
-    sun = loadImage("img/sun_emoji.png");
+  sun = loadImage("img/sun_emoji.png");
 }
 
 function setup() {
-    createCanvas(800, 600);
-    angle = PI/4;
-    planet = createVector(); // was bob
-    armLength = 250;
-    origin = createVector(400, 300);
-    slider = createSlider(0, 2, 0.05, 0.05); // first 0.05  is default, second is the step
-    slider.position(75, 320)
-    slider.style('width', '200px')
+  createCanvas(800, 600);
+  angle = PI/4;
+  planet = createVector(); // was bob
+  armLength = 250;
+  origin = createVector(400, 300);
+  slider = createSlider(0, 2, 0.05, 0.05); // first 0.05  is default, second is the step
+  slider.position(75, 320)
+  slider.style('width', '200px')
 }
 
 function draw() {
-    background(4, 4, 20);
-    image(sun, 350, 250);
+  background(4, 4, 20);
+  image(sun, 350, 250);
 
-    angle += angleVelocity * easing;
-    angleVelocity += angleAcceleration;
-    console.log("angle velocity", angleVelocity);
-    // before integrating angleVelocity and angleAcceleration, this was angle += 0.01;
-    if (mouseIsPressed == true) {
-        angleVelocity = 0;
-        fill(255, 0, 0);
-    } else {
-        fill(163, 44, 21);
-    }
-  
-    // To calculate the planet.x and planet.y:
-    // remember that sin Angle = y / z (z is the hypotenuse side)
-    planet.x = armLength * sin(angle) + origin.x; 
-    planet.y = armLength * cos(angle) + origin.y;
-    strokeWeight(0);
-    line(origin.x, origin.y, planet.x, planet.y);
+  angle += angleVelocity * easing;
+  angleVelocity += angleAcceleration;
+  console.log("angle velocity", angleVelocity);
+  // before integrating angleVelocity and angleAcceleration, this was angle += 0.01;
+  if (mouseIsPressed == true) {
+    angleVelocity = 0;
+    fill(255, 0, 0);
+  } else {
+    fill(163, 44, 21);
+  }
 
-    stroke('rgba(250, 131, 107, 0.3)');
-    strokeWeight(8);
-    ellipse(planet.x, planet.y, 65, 65);
-    
-    easing = slider.value();
-
+  // To calculate the planet.x and planet.y:
+  // sin Angle = y / z (z is the hypotenuse side in this formula)
+  planet.x = armLength * sin(angle) + origin.x; 
+  planet.y = armLength * cos(angle) + origin.y;
+  strokeWeight(0);
+  line(origin.x, origin.y, planet.x, planet.y);
+  stroke('rgba(250, 131, 107, 0.3)');
+  strokeWeight(8);
+  ellipse(planet.x, planet.y, 65, 65);
+  easing = slider.value();
 }
