66a2b42fde0248d1e01eb38888906e256a481751
Sorcha <64171410+egglady@users.noreply.github.com>
2021-04-30 19:14:02 -0500

    p5 shapes

---
+++ week14/script.js
@@ -0,0 +1,65 @@
+function setup() {
+    createCanvas(600, 400);
+    background(200);
+}
+
+function draw() {
+    // kiki
+    beginShape();
+    vertex(100, 100);
+    vertex(130, 122);
+    vertex(175, 60);
+    vertex(160, 145);
+    vertex(220, 142);
+    vertex(170, 170);
+    vertex(215, 220);
+    vertex(150, 195);
+    vertex(130, 250);
+    vertex(125, 190);
+    vertex(55, 225);
+    vertex(105, 168);
+    vertex(30, 135);
+    vertex(115, 145);
+    vertex(100, 100);
+    endShape();
+
+    // bouba
+    beginShape();
+    curveVertex(362, 145);
+    curveVertex(362, 145);
+    curveVertex(370, 70);
+    curveVertex(395, 65);
+    curveVertex(410, 136); //
+    curveVertex(412, 142); //
+    curveVertex(490, 95);
+    curveVertex(510, 130);
+    curveVertex(455, 185); //
+    curveVertex(460, 185); //
+    curveVertex(510, 200);
+    curveVertex(525, 225);
+    curveVertex(455, 235); //
+    curveVertex(450, 235); //
+    curveVertex(460, 300);
+    curveVertex(435, 315);
+    curveVertex(410, 250); //
+    curveVertex(410, 250); //
+    curveVertex(325, 275);
+    curveVertex(320, 240);
+    curveVertex(360, 205); //
+    curveVertex(360, 205); //
+    curveVertex(310, 180);
+    curveVertex(305, 155);
+    curveVertex(362, 145); //
+    curveVertex(362, 145); //
+    endShape();
+    
+
+//     beginShape();
+//     curveVertex(365, 110);
+//     curveVertex(367, 90);
+//     curveVertex(372, 70);
+//     curveVertex(370, 60);
+//     endShape();
+}
+
+
