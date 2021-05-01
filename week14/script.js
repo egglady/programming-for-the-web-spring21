6c2cab989bfc86e516b96ff8ccd9cc5b25c01363
Sorcha <64171410+egglady@users.noreply.github.com>
2021-05-01 09:50:30 -0500

    bouba shapes

--- week14/script.js
+++ week14/script.js
@@ -25,30 +25,34 @@ function draw() {
 
     // bouba
     beginShape();
-    curveVertex(362, 145);
-    curveVertex(362, 145);
+    curveVertex(362, 145); //
+    curveVertex(362, 145); //
     curveVertex(370, 70);
     curveVertex(395, 65);
-    curveVertex(410, 136); //
-    curveVertex(412, 142); //
-    curveVertex(490, 95);
-    curveVertex(510, 130);
-    curveVertex(455, 185); //
-    curveVertex(460, 185); //
-    curveVertex(510, 200);
-    curveVertex(525, 225);
+    curveVertex(410, 138); //
+    curveVertex(416, 142); //
+    curveVertex(480, 95);
+    curveVertex(500, 125);
+    curveVertex(455, 182); //
+    curveVertex(458, 184); //
+    curveVertex(510, 180);
+    curveVertex(525, 240);
     curveVertex(455, 235); //
-    curveVertex(450, 235); //
-    curveVertex(460, 300);
-    curveVertex(435, 315);
+    curveVertex(450, 237); //
+    curveVertex(460, 285);
+    curveVertex(435, 300);
     curveVertex(410, 250); //
-    curveVertex(410, 250); //
-    curveVertex(325, 275);
-    curveVertex(320, 240);
-    curveVertex(360, 205); //
-    curveVertex(360, 205); //
-    curveVertex(310, 180);
-    curveVertex(305, 155);
+    curveVertex(408, 245); //
+    curveVertex(360, 295);
+    curveVertex(350, 265);
+    curveVertex(365, 225); //
+    curveVertex(360, 222); //
+    curveVertex(308, 255);
+    curveVertex(306, 205);
+    curveVertex(350, 175); //
+    curveVertex(350, 172); //
+    curveVertex(305, 160)
+    curveVertex(325, 125);
     curveVertex(362, 145); //
     curveVertex(362, 145); //
     endShape();
